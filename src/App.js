af392a000ab53c1d19a3c435a0a8af69076a681f
Promish Gurung <promishgrg98@gmail.com>
2024-03-07 19:38:35 +0545

    Add Router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import './App.css';
 import NavBar from './components/NavBar';
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import Home from './pages/Home';
 
 function App() {
   return (
     <div className="App">
-      <NavBar />
+      <Router>
+       <NavBar />
+       <Routes>
+        <Route path='/' exact Component={Home} />
+       </Routes>
+      </Router>
     </div>
   );
 }
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import Logo from '../assets/pizzaLogo.png';
+import { Link } from 'react-router-dom';
 
 function NavBar() {
   return (
@@ -7,7 +8,12 @@ function NavBar() {
         <div className='leftSide'>
             <img src={Logo} />
         </div>
-        <div className='rightSide'></div>
+        <div className='rightSide'>
+          <Link to={'/'}>Home</Link>
+          <Link to={'/menu'}>Menu</Link>
+          <Link to={'/about'}>About</Link>
+          <Link to={'/contact'}>Contact</Link>
+        </div>
     </div>
   )
 }

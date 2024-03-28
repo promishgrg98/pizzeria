e48164f2a05f3cfc5cbea70293d2b74838819797
Promish Gurung <promishgrg98@gmail.com>
2024-03-28 13:56:20 +0545

    Add Menu

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import NavBar from './components/NavBar';
 import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import Home from './pages/Home';
 import Footer from './components/Footer';
+import Menu from './pages/Menu';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
        <NavBar />
        <Routes>
         <Route path='/' exact Component={Home} />
+        <Route path='/menu' exact Component={Menu} />
        </Routes>
        <Footer />
       </Router>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,7 +5,7 @@ import '../styles/Home.css';
 
 function Home() {
   return (
-    <div className='home'style={{backgroundImage: `url(${BannerImage})`}}>
+    <div className='home' style={{backgroundImage: `url(${BannerImage})`}}>
       <div className='headerContainer' >
         <h1>P's Pizzeria</h1>
         <p>PIZZA TO FIT ANY TASTE</p>
